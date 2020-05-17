5f006901a62b83a53dfb186a7647865ffe282667
zihe <he100zihao100@126.com>
2020-05-17 14:42:21 +0800

    增加AOP环绕Around实例。

--- src/main/java/com/zh/spring/aop/CarAspect.java
+++ src/main/java/com/zh/spring/aop/CarAspect.java
@@ -1,7 +1,9 @@
 package com.zh.spring.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -12,7 +14,7 @@ import org.springframework.stereotype.Component;
 //切面类也是Spring管理的Bean
 @Component
 @Aspect
-public class OutAspect {
+public class CarAspect {
     @Before("execution(public void out())")
     public void outBeforeNote(JoinPoint point) {
         //在出门前执行
@@ -23,5 +25,14 @@ public class OutAspect {
     public void outAfterNote(JoinPoint point) {
         //在出门后执行
         System.out.println("<========= 出门成功 ========>\n");
+    }
+
+    @Around("execution(public void out())")
+    public void aroundNote(ProceedingJoinPoint point) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        //执行原方法
+        point.proceed();
+        long endTime = System.currentTimeMillis();
+        System.out.println("出门耗时（毫秒）：" + (endTime - startTime));
     }
 }
